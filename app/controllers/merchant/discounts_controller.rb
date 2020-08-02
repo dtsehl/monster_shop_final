7c26a523472e44aa538aee2af18ebf8a0c9ceb90
Dan Sehl <dan.sehl@gmail.com>
2020-08-01 19:48:27 -0600

    Create #edit and #update actions for discount

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -23,6 +23,21 @@ class Merchant::DiscountsController < Merchant::BaseController
     end
   end
 
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    @discount = Discount.find(params[:id])
+    if @discount.update(discount_params)
+      flash[:notice] = "Discount Updated"
+      redirect_to "/merchant/discounts/#{@discount.id}"
+    else
+      flash[:error] = @discount.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
   private
 
   def discount_params
