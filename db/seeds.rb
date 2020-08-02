2af0c39acbdb732194200b96aa9a8b4cbe06c7b6
Dan Sehl <dan.sehl@gmail.com>
2020-08-02 17:53:00 -0600

    Add a discount to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 Merchant.destroy_all
 Item.destroy_all
-User.destroy_all
 Review.destroy_all
+Discount.destroy_all
 
 User.create!(name: 'Admin', address: '123 Who Cares Ln', city: 'Denver', state: 'CO', zip: '12345', email: 'admin@admin.com', password: 'admin', role: 2)
 User.create!(name: 'Default', address: '789 Hello St', city: 'Denver', state: 'CO', zip: '12345', email: 'default@default.com', password: 'default', role: 0)
@@ -22,3 +23,4 @@ megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image:
 megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 brian.items.create!(name: 'Clothes', description: "Clothes!", price: 20, image: 'https://upload.wikimedia.org/wikipedia/commons/b/b0/MTB_over_the_bar_crash.jpg', active: true, inventory: 10 )
+megan.discounts.create!(name: "5% off 3 or more items", min_item_quantity: 3, percent_off: 5)
