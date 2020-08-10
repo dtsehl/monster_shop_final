d55172e88f35e656c1525558f27f220ddb09cb0b
Dan Sehl <dan.sehl@gmail.com>
2020-08-10 10:58:50 -0600

    Refactor routes to use resources or handrolling where possible

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,35 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get :root, to: 'welcome#index'
 
-  resources :merchants do
-    resources :items, only: [:index]
-  end
+  # resources :merchants do
+  #   resources :items, only: [:index]
+  # end
 
-  resources :items, only: [:index, :show] do
-    resources :reviews, only: [:new, :create]
-  end
+  get 'merchants', to: 'merchants#index'
+  post 'merchants', to: 'merchants#create'
+  get 'merchants/new', to: 'merchants#new', as: :new_merchant
+  get 'merchants/:id/edit', to: 'merchants#edit', as: :edit_merchant
+  get 'merchants/:id', to: 'merchants#show'
+  patch 'merchants/:id', to: 'merchants#update'
+  delete 'merchants/:id', to: 'merchants#destroy'
 
-  resources :reviews, only: [:edit, :update, :destroy]
+  get 'merchants/:merchant_id/items', to: 'items#index', as: :merchant_items
+
+
+  # resources :items, only: [:index, :show] do
+  #   resources :reviews, only: [:new, :create]
+  # end
+
+  get 'items', to: 'items#index'
+  get 'items/:id', to: 'items#show', as: :item
+  get 'items/:item_id/reviews/new', to: 'reviews#new', as: :new_item_review
+  post 'items/:item_id/reviews', to: 'reviews#create', as: :item_reviews
+
+  # resources :reviews, only: [:edit, :update, :destroy]
+
+  get 'reviews/:id/edit', to: 'reviews#edit', as: :edit_review
+  patch 'reviews/:id', to: 'reviews#update', as: :review
+  delete 'reviews/:id', to: 'reviews#destroy'
 
   get '/cart', to: 'cart#show'
   post '/cart/:item_id', to: 'cart#add_item'
@@ -19,7 +39,9 @@ Rails.application.routes.draw do
   delete '/cart/:item_id', to: 'cart#remove_item'
 
   get '/registration', to: 'users#new', as: :registration
-  resources :users, only: [:create, :update]
+  # resources :users, only: [:create, :update]
+  post 'users', to: 'users#create', as: :users
+  patch 'users/:id', to: 'users#update', as: :user
   patch '/user/:id', to: 'users#update'
   get '/profile', to: 'users#show'
   get '/profile/edit', to: 'users#edit'
@@ -35,17 +57,38 @@ Rails.application.routes.draw do
 
   namespace :merchant do
     get '/', to: 'dashboard#index', as: :dashboard
-    resources :orders, only: :show
-    resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
-    resources :discounts
+    # resources :orders, only: :show
+    get 'orders/:id', to: 'orders#show', as: :order
+    # resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
+    get 'items', to: 'items#index'
+    post 'items', to: 'items#create'
+    get 'items/new', to: 'items#new'
+    get 'items/:id/edit', to: 'items#edit'
+    patch 'items/:id', to: 'items#update'
+    put 'items/:id', to: 'items#update'
+    delete 'items/:id', to: 'items#destroy'
+    # resources :discounts
+    get 'discounts', to: 'discounts#index'
+    get 'discounts/:id', to: 'discounts#show'
+    post 'discounts', to: 'discounts#create'
+    get 'discounts/new', to: 'discounts#new'
+    get 'discounts/:id/edit', to: 'discounts#edit'
+    patch 'discounts/:id', to: 'discounts#update'
+    put 'discounts/:id', to: 'discounts#update'
+    delete 'discounts/:id', to: 'discounts#destroy'
     put '/items/:id/change_status', to: 'items#change_status'
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
   end
 
   namespace :admin do
     get '/', to: 'dashboard#index', as: :dashboard
-    resources :merchants, only: [:show, :update]
-    resources :users, only: [:index, :show]
+    # resources :merchants, only: [:show, :update]
+    get 'merchants/:id', to: 'merchants#show', as: :merchant
+    patch 'merchants/:id', to: 'merchants#update'
+    put 'merchants/:id', to: 'merchants#update'
+    # resources :users, only: [:index, :show]
+    get 'users', to: 'users#index', as: :users
+    get 'users/:id', to: 'users#show', as: :user
     patch '/orders/:id/ship', to: 'orders#ship'
   end
 end
